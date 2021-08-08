0892df5a9d7264d0c9e5bd26be3eb60a988c0c61
mparedes <mparedes@mparedes.mx>
2021-08-08 18:58:06 -0500

    Init Firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBwTfhotAjCrej1iZiql3yLm6hVAeSj_lc",
+  authDomain: "fb-crud-react-e10f2.firebaseapp.com",
+  projectId: "fb-crud-react-e10f2",
+  storageBucket: "fb-crud-react-e10f2.appspot.com",
+  messagingSenderId: "165748611836",
+  appId: "1:165748611836:web:4b484b70f34c5315419579"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
