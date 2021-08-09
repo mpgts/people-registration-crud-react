1eed436a63294218f24aa34048b55bf96196ab84
mparedes <mparedes@mparedes.mx>
2021-08-09 14:14:32 -0500

    Create comp Links LinkForm

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
 import './App.css';
+import Links from './components/Links';
+import LinkForm from './components/LinkForm';
 
 function App() {
   return (
-    <h1>Firebase</h1>
+    <>
+      <LinkForm />
+      <Links />
+    </>
   );
 }
 
---
+++ src/components/LinkForm.js
@@ -0,0 +1,7 @@
+import React, { PureComponent } from 'react';
+
+const LinkForm = () => {
+    return <h1>LinkForm</h1>
+}
+
+export default LinkForm;
